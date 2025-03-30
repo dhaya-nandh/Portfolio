03aae095d4bada862541b05869f0471cf6aa247b
Dhayanandh <159157709+dhaya-nandh@users.noreply.github.com>
2025-03-30 19:04:05 +0530

    incomplete

---
+++ Portfolio/script.js
@@ -0,0 +1,6 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for your message! I'll get back to you soon.");
+    this.reset();
+  });
+  
