08c2410912f9dc8bba96b45d01c6b18e1a5e5857
dhaya-nandh <dhayanandh510@gmail.com>
2025-04-03 14:41:17 +0530

    ready

--- Portfolio/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Thank you for your message! I'll get back to you soon.");
-    this.reset();
-  });
-  
